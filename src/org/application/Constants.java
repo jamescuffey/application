2f293a88f89c73192df9dc769558f082c272460b
jamescuffey <passionsdeluxe@hotmail.co.uk>
2014-11-23 21:01:29 +0000

    Update Constants.java

--- src/org/application/Constants.java
+++ src/org/application/Constants.java
@@ -10,23 +10,24 @@ public class Constants {
 	/**
 	 * Our tick delay.
 	 */
-	public static int TICK_DELAY = 1200;
+	public static final int TICK_DELAY = 1200;
 	
 	/**
 	 * The amount of items sold in the auction.
 	 */
-	public static int AUCTION_AMOUNT = 100;
+	public static final int AUCTION_AMOUNT = 100;
 	
 	/**
 	 * The minimum price of an item.
 	 */
-	public static int MINIMUM_PRICE = 5600;
+	public static final int MINIMUM_PRICE = 5600;
 	
 	/**
 	 * The minimum amount sold the item.
 	 */
-	public static int MINIMUM_AMOUNT = 20;
+	public static final int MINIMUM_AMOUNT = 20;
 	
+	//stop instantation of this class.
 	private Constants() {
 		
 	}
